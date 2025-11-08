009217089354734cb1805e38e30f7611702de4f2
Ricardo Augusto <ricxd_@hotmail.com>
2025-11-08 00:19:50 -0300

    feat: implementation in CathegoryHandler

--- Fund.Api/Handlers/CategoryHandler.cs
+++ Fund.Api/Handlers/CategoryHandler.cs
@@ -3,6 +3,9 @@ using Fund.Core.Handlers;
 using Fund.Core.Models;
 using Fund.Core.Requests.Categories;
 using Fund.Core.Responses;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Security.Cryptography.Xml;
 
 namespace Fund.Api.Handlers
 {
@@ -10,27 +13,117 @@ namespace Fund.Api.Handlers
     {
         public async Task<Response<Category?>> CreateAsync(CreateCategoryRequest request)
         {
-            return new Response<Category?>(data:null,code:201,message:"Categoria criada com sucesso!" );
+            var category = new Category
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Description = request.Description
+            };
+
+            await context.Categories.AddAsync(category);
+
+            try
+            {
+                await context.SaveChangesAsync();
+
+                return new Response<Category?>(data: null, code: 201, message: "Categoria criada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return new Response<Category?>(data: null, code: 500, message: "Não foi possível criar uma categoria.");
+            }
         }
 
         public async Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
         {
-            throw new NotImplementedException();
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (category is null)
+            {
+                return new Response<Category?>(data: null, code: 404, message: "Categoria não encontrada.");
+            }
+
+            context.Categories.Remove(category);
+
+            try
+            {
+                await context.SaveChangesAsync();
+                return new Response<Category?>(data: null, message: "Categoria excluída com sucesso!");
+
+            }
+            catch (Exception ex)
+            {
+                return new Response<Category?>(data: null, code: 500, message: "Não foi possível excluir a categoria.");
+            }
+
         }
 
         public async Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoriesRequest request)
         {
-            throw new NotImplementedException();
+            var query = context.Categories.AsNoTracking().Where(x => x.UserId == request.UserId).OrderBy(x => x.Title);
+
+            var categories = await query
+                .Skip(request.PageNumber - 1 * request.PageSize)
+                .Take(request.PageSize)
+                .ToListAsync();
+
+            try
+            {
+                var count = await query.CountAsync();
+
+                return new PagedResponse<List<Category>?>(
+                    categories,
+                    count,
+                    request.PageNumber,
+                    request.PageSize);
+            }
+            catch 
+            {
+                return new PagedResponse<List<Category>?>(data: null, code: 500, message: "Não foi possível recuperar todas as categorias.");
+            }
+
         }
 
         public async Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+                return category is null
+                ? new Response<Category?>(data: null, code: 404, message: "Categoria não encontrada.")
+                : new Response<Category?>(category);
+            }
+            catch (Exception ex)
+            {
+                return new Response<Category>(data: null, code: 500, message: "Não foi possível recuperar a categoria.");
+            }
+
         }
 
         public async Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
         {
-            throw new NotImplementedException();
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (category is null)
+            {
+                return new Response<Category?>(data: null, code: 404, message: "Categoria não encontrada.");
+            }
+
+            category.Title = request.Title;
+            category.Description = request.Description;
+            context.Categories.Update(category);
+
+            try
+            {
+                await context.SaveChangesAsync();
+
+                return new Response<Category?>(category, message: "Categoria atualizada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return new Response<Category>(data: null, code: 500, message: "Não foi possível atualizar a categoria.");
+            }
         }
     }
 }
--- Fund.Api/Handlers/TransactionHandler.cs
+++ Fund.Api/Handlers/TransactionHandler.cs
@@ -3,6 +3,7 @@ using Fund.Core.Handlers;
 using Fund.Core.Models;
 using Fund.Core.Requests.Transactions;
 using Fund.Core.Responses;
+using Fund.Core.Enums;
 
 namespace Fund.Api.Handlers
 {
@@ -10,7 +11,30 @@ namespace Fund.Api.Handlers
     {
         public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
         {
-            throw new NotImplementedException();
+            if (request is { Type: Fund.Core.Enums.ETransactionType.Withdraw, Amount: > 0 })
+                request.Amount *= -1;
+
+            var transaction = new Transaction
+            {
+                UserId = request.UserId,
+                CategoryId = request.CategoryId,
+                CreatedAt = DateTime.Now,
+                Amount = request.Amount,
+                PaidOrReceivedAt = request.PaidOrReceivedAt,
+                Title = request.Title,
+                Type = request.Type
+            };
+
+            try
+            {
+                await context.Transactions.AddAsync(transaction);
+                await context.SaveChangesAsync();
+                return new Response<Transaction?>(transaction, code: 201, message: "Transação criada com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                return new Response<Transaction?>(data: null, code: 500, message: "Não foi possível criar sua transação.");
+            }
         }
 
         public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
